fa16fea73ec3897bf6353f9b4b377225409e9996
Ian Fisher <iafisher@protonmail.com>
2018-07-17 20:41:37 -0400

    Implement DailyWriting.__str__

--- compose/models.py
+++ compose/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class DailyWriting(models.Model):
     date = models.DateField()
     text = models.TextField()
+
+    def __str__(self):
+        return 'Daily Writing, ' + self.date.isoformat()

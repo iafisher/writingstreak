57bfde134ff002cae4d79ae115b24282070ae99f
Ian Fisher <iafisher@protonmail.com>
2018-08-17 08:25:45 -0400

    Add some tests of model functionality

--- compose/tests.py
+++ compose/tests.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import resolve
-from . import views
+from . import models, views
 
 
 class TestURLs(TestCase):
@@ -58,3 +60,31 @@ class TestRedirects(TestCase):
     def test_index_page_redirects(self):
         response = self.client.get('/')
         self.assertRedirects(response, '/login/?next=/')
+
+
+class TestModels(TestCase):
+    def test_word_count_updated_on_save(self):
+        User = get_user_model()
+        u = User.objects.create_user(TEST_USER, 'temporary@example.com',
+            TEST_PWD)
+        entry = models.DailyEntry(date=datetime.date.today(),
+            text='Lorem ipsum', user=u)
+
+        entry.save()
+
+        self.assertEqual(entry.word_count, 2)
+
+    def test_calculate_word_count(self):
+        entry = models.DailyEntry(text=' so--if  the minister-president is...')
+        self.assertEqual(5, entry.calculate_word_count())
+
+    def test_get_absolute_url(self):
+        User = get_user_model()
+        u = User.objects.create_user(TEST_USER, 'temporary@example.com',
+            TEST_PWD)
+        date = datetime.date(year=2018, month=8, day=1)
+        entry = models.DailyEntry(date=date, text='Lorem ipsum', user=u)
+
+        url = entry.get_absolute_url()
+
+        self.assertEqual(url, '/archive/2018/8/1')
